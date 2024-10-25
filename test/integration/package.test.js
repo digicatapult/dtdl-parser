cd97e771c5f409a0dce8b64816049d0ac0578af5
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2024-10-25 16:49:23 +0100

    tests updated
    using dtdl/simple ontology

--- test/integration/package.test.js
+++ test/integration/package.test.js
@@ -1,12 +1,38 @@
 import { expect } from 'chai'
+import { describe, it } from 'mocha'
 import path from 'node:path'
-
 import { getInterop, parseDirectories } from '../../build/index.js'
 
-const fixturesFilepath = path.resolve('src/__tests__/fixtures')
+const fixturesFilepath = path.resolve('dtdl/simple')
+
+const emptyEntityProperties = {
+  SupplementalTypes: [],
+  SupplementalProperties: {},
+  UndefinedTypes: [],
+  UndefinedProperties: {},
+  description: {},
+  languageMajorVersion: 2,
+  displayName: {},
+  ChildOf: null,
+  DefinedIn: null,
+}
+const emptyInterfaceProperties = {
+  contents: {},
+  commands: {},
+  components: {},
+  properties: {},
+  relationships: {},
+  telemetries: {},
+  extendedBy: [],
+  extends: [],
+  schemas: [],
+  comment: null,
+}
 
 const exampleModel = {
   'dtmi:com:example:base;1': {
+    ...emptyEntityProperties,
+    ...emptyInterfaceProperties,
     languageMajorVersion: 3,
     Id: 'dtmi:com:example:base;1',
     ChildOf: 'dtmi:com:example;1',
@@ -16,6 +42,8 @@ const exampleModel = {
     extendedBy: ['dtmi:com:example;1'],
   },
   'dtmi:com:example;1': {
+    ...emptyEntityProperties,
+    ...emptyInterfaceProperties,
     languageMajorVersion: 3,
     Id: 'dtmi:com:example;1',
     EntityKind: 'Interface',
@@ -29,7 +57,7 @@ describe('integration test on build and package integrity', () => {
     it('parser to function as expected', async () => {
       const parser = await getInterop()
       const model = parseDirectories(fixturesFilepath, parser)
-      expect(model).to.equal(exampleModel)
+      expect(model).to.deep.equal(exampleModel)
     })
   })
 })
